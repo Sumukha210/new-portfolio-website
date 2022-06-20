aae38f6a6558c5332ffd78e9de5672cc9210de44
Sumukha210 <sumukhakb@gmail.com>
2022-06-20 17:44:25 +0530

    escape is added to skills section

--- src/components/modules/skills/Skills.tsx
+++ src/components/modules/skills/Skills.tsx
@@ -24,20 +24,21 @@ const Skills = () => {
               <div className="left">
                 <Title name="My Skills" />
                 <p className="subtitle-5 description">
-                  Since web technologies always move at a rapid pace, I'm always
-                  eager to learn different technologies and build projects using
-                  those technologies.
+                  Since web technologies always move at a rapid pace, I&rsquo;m
+                  always eager to learn different technologies and build
+                  projects using those technologies.
                 </p>
                 <p className="subtitle-5 description">
-                  Also, if the project that I'm working for the company needs
-                  different tools, I'm willing to learn those things. When
-                  working with teams, I tailor my approach to your needs. One
-                  tool does not work for everybody, and I always aim to achieve
-                  the best results for who I’m currently working with.
+                  Also, if the project that I&rsquo;m working for the company
+                  needs different tools, I&rsquo;m willing to learn those
+                  things. When working with teams, I tailor my approach to your
+                  needs. One tool does not work for everybody, and I always aim
+                  to achieve the best results for who I&rsquo;m currently
+                  working with.
                 </p>
                 <p className="subtitle-5 description">
-                  Here's just a few of the Technologies / Tools I've worked with
-                  in the past:-
+                  Here&rsquo;s just a few of the Technologies / Tools I&rsquo;ve
+                  worked with in the past:-
                 </p>
               </div>
 
