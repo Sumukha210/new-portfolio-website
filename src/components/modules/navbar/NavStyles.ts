6370a99f43a6e59e18b34e2c1b5412fb09d380ae
Sumukha210 <sumukhakb@gmail.com>
2022-06-20 18:26:08 +0530

    navbar is done

--- src/components/modules/navbar/NavStyles.ts
+++ src/components/modules/navbar/NavStyles.ts
@@ -1,14 +1,22 @@
 import styled from "styled-components";
 
 export const Nav = styled.div`
+  --navBackgroundColor: rgba(0, 0, 0, 0.2);
+  --navBlur: blur(6px);
+
+  @media (max-width: 767.98px) {
+    --navBackgroundColor: rgba(0, 0, 0, 0.95);
+    --navBlur: blur(20px);
+  }
+
   position: fixed;
   top: 0%;
   left: 0%;
   width: 100%;
   z-index: 100;
 
-  background: rgba(0, 0, 0, 0.2);
-  backdrop-filter: blur(6px);
+  background: var(--navBackgroundColor);
+  backdrop-filter: var(--navBlur);
 `;
 
 export const Container = styled.div`
@@ -29,9 +37,15 @@ export const Container = styled.div`
     }
   }
 
+  .mobileMenu {
+    display: none;
+  }
+
   .navMenu {
-    display: flex;
-    align-items: center;
+    ul {
+      display: flex;
+      align-items: center;
+    }
 
     li {
       list-style: none;
@@ -57,6 +71,67 @@ export const Container = styled.div`
       &:not(:first-of-type) {
         margin-left: 2rem;
       }
+    }
+  }
+
+  /* on small screen */
+  @media (max-width: 767.98px) {
+    .mobileMenu {
+      display: block;
+
+      svg {
+        height: 25px;
+        width: 30px;
+      }
+    }
+
+    .navMenu {
+      position: absolute;
+      top: 60px;
+      right: -150%;
+      height: 100vh;
+      width: 100%;
+      padding-left: 24px;
+      padding-right: 24px;
+      transition: right 0.7s ease-out;
+
+      background: var(--navBackgroundColor);
+      backdrop-filter: var(--navBlur);
+
+      ul {
+        flex-direction: column;
+        align-items: flex-start;
+        padding-top: 5rem;
+
+        width: 540px;
+        margin-inline: auto;
+      }
+
+      li {
+        margin-left: 0px !important;
+        font-size: 2.369rem; // 37px
+        line-height: 50.52px;
+        font-family: var(--primary-font);
+        letter-spacing: 1px;
+
+        margin-bottom: 3rem;
+      }
+    }
+
+    @media (max-width: 575.98px) {
+      .navMenu {
+        ul {
+          padding-top: 2.5rem;
+          width: 90%;
+        }
+      }
+    }
+  }
+
+  &.mobileMenuOpen {
+    .navMenu {
+      right: 0%;
+      transition: right 0.7s ease-out;
     }
   }
 `;
--- src/components/modules/navbar/Navbar.tsx
+++ src/components/modules/navbar/Navbar.tsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Nav } from "./NavStyles";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setMobileOpen] = useState(false);
+
   return (
     <Nav>
       <div className="container">
         <div className="inner-container">
-          <Container>
+          <Container className={`${isMobileMenuOpen ? "mobileMenuOpen" : ""}`}>
             <div className="logo subtitle-1">
               <span>KBS.</span>
             </div>
 
-            <ul className="navMenu">
-              <li className="subtitle-5">Home</li>
-              <li className="subtitle-5">About</li>
-              <li className="subtitle-5">Project</li>
-              <li className="subtitle-5">Blog</li>
-              <li className="subtitle-5">Contact</li>
-            </ul>
+            <div className="navMenu">
+              <ul>
+                <li className="subtitle-5">Home</li>
+                <li className="subtitle-5">About</li>
+                <li className="subtitle-5">Project</li>
+                <li className="subtitle-5">Blog</li>
+                <li className="subtitle-5">Contact</li>
+              </ul>
+            </div>
+
+            <div
+              className="mobileMenu"
+              onClick={() => setMobileOpen(prevState => !prevState)}>
+              {isMobileMenuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+            </div>
           </Container>
         </div>
       </div>
